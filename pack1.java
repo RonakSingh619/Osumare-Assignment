cad53a0cc3138df7242d3f52374b1fb0d95b9bb7
RonakSingh619 <ronaksinghJC@gmail.com>
2025-04-29 19:22:44 +0530

    Delete pack1.java

--- pack1.java
+++
@@ -1,9 +0,0 @@
-
-package tatya;
-
-public class pack1 {
-    public static void main(String[] args){
-        System.out.println("-----------------------------------------\n" +
-                "This is tatya package class pack1\n-----------------------------------------");
-    }
-}
