b5e3e1cd4c02f415dc761fe451f655049ff61f3c
Your Name <your.email@example.com>
2025-04-29 19:05:45 +0530

    Vs push

---
+++ pack.java
@@ -0,0 +1,14 @@
+
+package tatya;
+
+public class pack {
+
+    public void msg(){
+        System.out.println("Hello boi this is msg");
+    }
+
+    public static void main(String[] args){
+        System.out.println("-----------------------------------------\n" +
+                "This is tatya package class\n-----------------------------------------");
+    }
+}
